04ea2327df61b0c7d69f9ed2e1ea9f8b287b5a66
Em01 <emma_williams1@hotmail.co.uk>
2014-11-29 16:29:19 +0000

    working on max page loop

--- airbnb_scraper.rb
+++ airbnb_scraper.rb
@@ -16,30 +16,34 @@ end
 
 max_page = page_numbers.max
 
-puts max_page
+max_page.to_i.times do |i|
+
+	url = "https://www.airbnb.com/s/Brooklyn--NY--United-States?page=#{i+1}"
+	page = Nokogiri::HTML(open(url))
 
 #Store data in arrays
-name = []
-page.css('div.h5.listing-name').each do |line|
-	name << line.text.strip
-end
+	name = []
+	page.css('div.h5.listing-name').each do |line|
+		name << line.text.strip
+	end
 
-price = []
-page.css('span.h3.price-amount').each do |line|
-	price << line.text
-end
+	price = []
+	page.css('span.h3.price-amount').each do |line|
+		price << line.text
+	end
 
-details = []
-page.css('div.text-muted.listing-location.text-truncate').each do |line|
-	details << line.text.strip.split(/ · /)
+	details = []
+	page.css('div.text-muted.listing-location.text-truncate').each do |line|
+		details << line.text.strip.split(/ · /)
+	end
 end
 
 #write data to CSV file
-CSV.open("airbnb_listings.csv", "w") do |file|
-	file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
-	name.length.times do |i|
-		file << [name[i], price[i], details[i][0], details[i][1], details[i][2]]
-	end
+# CSV.open("airbnb_listings.csv", "w") do |file|
+# 	file << ["Listing Name", "Price", "Room Type", "Reviews", "Location"]
+# 	name.length.times do |i|
+# 		file << [name[i], price[i], details[i][0], details[i][1], details[i][2]]
+# 	end
 end
 
 
